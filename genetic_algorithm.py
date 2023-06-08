c897c9a985c75f9d6a63ebc602598d440d010561
Bryan LEGRAIN <brylegrain@gmail.com>
2023-06-08 14:41:55 +0200

    add create_population and selection in class Population

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,19 +1,54 @@
 import random
+from math import sqrt
 random.seed(42)
 
 class Population:
-    def __init__(self, value) -> None:
+    def __init__(self, value, nb_nodes) -> None:
         self.value = value
+        self.nb_nodes = nb_nodes
         self.population = []
+        self.nodes = self.create_nodes_list()
 
-    def create_population(self, nb_nodes):
+    def create_nodes_list(self) -> list:
         positions = []
-        for i in range(nb_nodes):
-            positions.append({i +1: (random.randrange(5000), random.randrange(5000))})
-        random.shuffle(positions)
+        for i in range(self.nb_nodes):
+            positions.append({i + 1: (random.randrange(5000), random.randrange(5000))})
         return positions
 
+    def create_population(self, nb_paths) -> list:
+        paths = []
+        nodes = self.nodes
+        for i in range(nb_paths):
+            path = []
+            random.shuffle(nodes)
+            path += nodes
+            path.insert(0, self.value)
+            paths.append(path)
+        self.population = paths
+        return paths
 
-root = Population({0, (0, 0)})
-print(root.create_population(4))
+    def selection(self) -> list:
+        population = self.population
+        distances = []
+        selection = []
+        for path in population:
+            distance = 0
+            temp_list = []
+            for point in path:
+                position = []
+                for value in point.values():
+                    position = value
 
+                if len(temp_list) > 0:
+                    distance += sqrt((position[0] - temp_list[0])**2 + (position[1] - temp_list[1])**2)
+                temp_list = position
+            distances.append(distance)
+        copy_distances = distances.copy()
+        copy_distances.sort()
+        for i in range(0, len(self.population)//3):
+            index = distances.index(copy_distances[i])
+            selection.append(population[index])
+        self.population = selection
+    
+root = Population({0: (0, 0)}, 4)
+root.create_population(10)
