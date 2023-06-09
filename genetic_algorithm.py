c51acfe45633beb5f3b4b2a956232b3fd84d3c95
Bryan LEGRAIN <brylegrain@gmail.com>
2023-06-09 13:34:34 +0200

    update get_distances, selection, crossbreeding and add mutation, evolution and get_shortest_path in class Population

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -27,10 +27,9 @@ class Population:
         self.population = paths
         return paths
 
-    def selection(self) -> list:
+    def get_distances(self) -> list:
         population = self.population
         distances = []
-        selection = []
         for path in population:
             distance = 0
             temp_list = []
@@ -43,12 +42,25 @@ class Population:
                     distance += sqrt((position[0] - temp_list[0])**2 + (position[1] - temp_list[1])**2)
                 temp_list = position
             distances.append(distance)
+        return distances
+    
+    def get_shortest_path(self):
+        distances = self.get_distances()
+        population = self.population
+        index = distances.index(distances[0])
+        return population[index], distances[0]
+    
+    def selection(self) -> list:
+        selection = []
+        distances = self.get_distances()
+        population = self.population
         copy_distances = distances.copy()
         copy_distances.sort()
         for i in range(0, len(self.population)//3):
             index = distances.index(copy_distances[i])
             selection.append(population[index])
         self.population = selection
+        return self.population
     
     def crossbreeding(self) -> list:
         crossbreeding = []
@@ -63,22 +75,43 @@ class Population:
         for _ in range(2):
             new_path = []
             for path in population:
-                indexes = [x for x in range(0, len(population))]
-                indexes.remove(population.index(path))
-                first_parent = takes_half(path)
-                second_parent = population[random.choice(indexes)]
-                for i in range(len(path)):
-                    if second_parent[i] not in first_parent:
-                        first_parent.append(second_parent[i])
-                new_path.append(first_parent) 
+                if population.index(path) != 0:
+                    indexes = [x for x in range(0, len(population))]
+                    indexes.remove(population.index(path))
+                    first_parent = takes_half(path)
+                    second_parent = population[random.choice(indexes)]
+                    for i in range(len(path)):
+                        if second_parent[i] not in first_parent:
+                            first_parent.append(second_parent[i])
+                    new_path.append(first_parent) 
+                else:
+                    new_path.append(path)
             crossbreeding += new_path
-        
-        self.population.append(crossbreeding)
+        self.population += crossbreeding
+        return self.population
     
-root = Population({0: (0, 0)}, 4)
-root.create_population(10)
-print(root.population)
-root.selection()
-print(root.population)
-root.crossbreeding()
-print(root.population)
+    def mutation(self, mutation_probability) -> list:
+        population = self.population
+        for path in population:
+            if population.index(path) != 0:
+                indexes = [x for x in range(1, 5)]
+                if random.randint(0, 100) <= mutation_probability:
+                    index_1 = random.choice(indexes)
+                    indexes.remove(index_1)
+                    index_2 = random.choice(indexes)
+                    path[index_1], path[index_2] = path[index_2], path[index_1]
+        return population
+
+    def evolution(self, mutation_probability, nb_cycle):
+        if nb_cycle > 0:
+            self.selection()
+            self.crossbreeding()
+            self.mutation(mutation_probability)
+            self.evolution(mutation_probability, nb_cycle - 1)
+        return self.get_shortest_path()
+            
+
+
+root = Population({0: (0, 0)}, 5)
+root.create_population(1000)
+print(root.evolution(5, 100))
